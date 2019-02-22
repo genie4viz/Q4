006b1282cef33f6a166f412184057a30dbaf74cc
selistym <selistym@protonmail.com>
2019-02-22 04:24:25 -0800

    completed 80%

--- heatmap.js
+++ heatmap.js
@@ -17,81 +17,127 @@ var svg = d3.select("body")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("heatmap.csv").then(function (data) {
+var selected_year = "2011";
 
-    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-    var crimeTypes = d3.map(data, function (d) {
-        return d.CrimeType;
-    }).keys();
-    var boroughs = ["Bronx", "Brooklyn" ,"Manhattan" ,"Queens", "Staten Island"];
+var years = ["2011", "2012", "2013", "2014", "2015"];
+//add option   
+var select = d3.select('body')
+    .append('select')
+    .attr('class', 'select')
+    .on('change', onchange);
+select
+    .selectAll('option')
+    .data(years).enter()
+    .append('option')
+    .text(function (d) {
+        return d;
+    });
+onchange();
 
-    // Build X scales and axis:
-    var x = d3.scaleBand()
-        .range([0, width])
-        .domain(crimeTypes)
-        .padding(0.05);
-    svg.append("g")
-        .style("font-size", 15)
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSize(0))
-        .select(".domain").remove();
+function onchange() {
+    selected_year = d3.select('select').property('value');
+    //Read the data
+    d3.csv("heatmap.csv").then(function (data) {
 
-    // Build Y scales and axis:
-    var y = d3.scaleBand()
-        .range([height, 0])
-        .domain(boroughs)
-        .padding(0.05);
-    svg.append("g")
-        .style("font-size", 15)
-        .call(d3.axisLeft(y).tickSize(0))
-        .select(".domain").remove();
+        data = reshape(data, selected_year);
+        
+        var boroughs = d3.map(data, function (d) {
+            return d.borough;
+        }).keys();
+        var crimes = d3.map(data, function (d) {
+            return d.crimetype;
+        }).keys();
+        var times = d3.map(data, function (d) {
+            return d.times;
+        }).keys();
 
-    // Build color scale
-    var myColor = d3.scaleSequential()
-        .interpolator(d3.interpolateInferno)
-        .domain([1, 100]);
+        
+        // Build X scales and axis:
+        var x = d3.scaleBand()
+            .range([0, width])
+            .domain(crimes)
+            .padding(0.05);
+        svg.append("g")
+            .style("font-size", 15)
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSize(0))
+            .select(".domain").remove()
 
-    // add the squares
-    svg.selectAll()
-        .data(data, function (d) {
-            console.log(d.CrimeType + ':' + d.Bronx);
-            return d.CrimeType + ':' + d.Bronx;
-        })
-        .enter()
-        .append("rect")
-        .attr("x", function (d) {
-            return x(d.CrimeType)
-        })
-        .attr("y", function (d) {
-            return y(d.Bronx)
-        })
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("width", x.bandwidth())
-        .attr("height", y.bandwidth())
-        .style("fill", function (d) {
-            return myColor(d.value)
-        })
-        .style("stroke-width", 4)
-        .style("stroke", "none")
-        .style("opacity", 0.8)
-})
+        // Build Y scales and axis:
+        var y = d3.scaleBand()
+            .range([height, 0])
+            .domain(boroughs)
+            .padding(0.05);
+        svg.append("g")
+            .style("font-size", 15)
+            .call(d3.axisLeft(y).tickSize(0))
+            .select(".domain").remove();
 
-// Add title to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -50)
-    .attr("text-anchor", "left")
-    .style("font-size", "22px")
-    .text("A d3.js heatmap");
+        // Build color scale
+        // console.log(Math.max(parseInt(times)));
+        var colors = d3.scaleLinear()
+            .range(["white", "#49006a"])
+            .domain([0, Math.max(parseInt(times))]);
 
-// Add subtitle to graph
-svg.append("text")
-    .attr("x", 0)
-    .attr("y", -20)
-    .attr("text-anchor", "left")
-    .style("font-size", "14px")
-    .style("fill", "grey")
-    .style("max-width", 400)
-    .text("A short description of the take-away message of this chart.");
+        // add the squares
+        svg.selectAll()
+            .data(data, function (d) {
+                return d.borough + ':' + d.crimetype;
+            })
+            .enter()
+            .append("rect")
+            .attr("x", function (d) {
+                return x(d.crimetype);
+            })
+            .attr("y", function (d) {
+                return y(d.borough);
+            })
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .attr("width", x.bandwidth())
+            .attr("height", y.bandwidth())
+            .style("fill", function (d) {
+                return colors(d.times);
+            })
+            .style("stroke-width", 4)
+            .style("stroke", "none")
+            .style("opacity", 0.8);
+
+        // Add title to graph
+        svg.append("text")
+            .attr("x", 0)
+            .attr("y", -50)
+            .attr("text-anchor", "left")
+            .style("font-size", "22px")
+            .text("A d3.js heatmap");
+
+        // Add subtitle to graph
+        svg.append("text")
+            .attr("x", 0)
+            .attr("y", -20)
+            .attr("text-anchor", "left")
+            .style("font-size", "14px")
+            .style("fill", "grey")
+            .style("max-width", 400)
+            .text("A short description of the take-away message of this chart.");
+    });
+};
+
+function reshape(data, year) {
+    var boroughs = data.columns.slice(0, data.columns.length - 2);
+    var reshapes = [];
+    for (var i = 0; i < boroughs.length; i++) {
+        for (var k = 0; k < data.length; k++) {
+            if (data[k]["Year"] == year) {
+                var item = {
+                    borough: boroughs[i]
+                };
+                item.crimetype = data[k]["Crime Type"];
+                item.times = data[k][boroughs[i]];
+                item.year = data[k]["Year"];
+                reshapes.push(item);
+            }
+        }
+    }
+    return reshapes;
+}
